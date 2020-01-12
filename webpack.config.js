a20e564df2a0e1b88d001cbb6a6696f6ab2599ed
VincentCSS <VincentHTML@gmail.com>
2020-01-11 21:44:11 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    open: true,
+    clientLogLevel: 'silent',
+    contentBase: './dist',
+    hot: true,
+  },
+};
